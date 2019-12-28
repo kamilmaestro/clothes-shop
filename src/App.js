2864c5256cd8ed32cea90a18596b4c9cb46fbe49
Kamil Marnik <ksdekamil@gmail.com>
2019-12-28 17:21:27 +0100

    collection-preview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import { HomePage, HatsPage } from './pages/homepage/HomePage';
+import { HomePage } from './pages/homepage/HomePage';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
