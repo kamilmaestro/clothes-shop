118f87ecf9767fbe95ac0463d7e164fec0730081
Kamil Marnik <ksdekamil@gmail.com>
2019-12-31 15:26:52 +0100

    tag a without decoration

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { HomePage } from './pages/homepage/HomePage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
