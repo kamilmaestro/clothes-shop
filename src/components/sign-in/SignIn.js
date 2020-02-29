6bad56dda251354ce9c89241e13e3fd6aeb05e67
kamilmaestro <ksdekamil@gmail.com>
2020-02-29 13:43:33 +0100

    signIn with loging via email

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -3,7 +3,7 @@ import './SignIn.styles.scss';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -15,12 +15,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: '',
-      password: ''
-    });
+    const { email, password } = this.state;  
+    try {
+      auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        'email': '',
+        'password': ''
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   handleChange = event => {
