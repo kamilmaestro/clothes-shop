ade1a593d2b8f934793636e614182bcd9e98bfe8
kamilmaestro <ksdekamil@gmail.com>
2020-02-29 13:38:10 +0100

    sign-up

--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.js
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SignInAndSignUpPage.styles.scss';
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 const SignInAndSignUpPage = () => {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn/>
+      <SignUp/>
     </div>
   );
 }
